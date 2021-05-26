abdbf462fa3addb59c8132db9f4c33fe858cb866
melvinlee770 <melvinlee770@gmail.com>
2021-05-27 00:43:24 +0800

    login_page

--- App.js
+++ App.js
@@ -7,21 +7,39 @@
  */
 
 import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import Background from './img/big.jpeg'
 
-import {
-  Text,
-  View,
-} from 'react-native';
-
-
-const App = () => {
-
-  return (
-    <View>
-      <Text>HelloWorld</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <ImageBackground source={Background} style={styles.container}>
+        <View>
+        <Text style={styles.app_name}>WetMart</Text>
+        <Text style={styles.app_ver}>Merchant</Text>
+        </View>
+      </ImageBackground>
+    );
+  }
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  app_name: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 70,
+    color: '#86B7B9'
+  },
+  app_ver: {
+    textAlign: 'center',
+    marginBottom: 400,
+    fontSize: 20
+  }
+})
 
-export default App;
+
