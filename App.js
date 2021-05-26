994ec492a44b000ddfb382fb52545c6d38606b68
melvinlee770 <melvinlee770@gmail.com>
2021-05-26 23:00:03 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>HelloWorld</Text>
+    </View>
+  );
+};
+
+
+export default App;
