89ab5832e5d61358bca9b18c994ee243bb8ca8c6
melvinlee770 <melvinlee770@gmail.com>
2021-06-20 01:10:07 +0800

    update

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, ImageBackground, FlatList, SafeAreaView, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Text, ImageBackground, FlatList, SafeAreaView, TouchableOpacity, Modal } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faWallet } from '@fortawesome/free-solid-svg-icons'
-import { onChange, set } from 'react-native-reanimated';
-import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
+import { faPlayCircle, faWallet, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+// import { onChange, set } from 'react-native-reanimated';
+// import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
 import { Card } from 'react-native-shadow-cards'
 import Background from '../img/big.jpeg'
-import { WellArchitected } from 'aws-sdk';
+import { parse } from '@babel/core';
+// import Animated, { color } from 'react-native-reanimated';
+// import { WellArchitected } from 'aws-sdk';
 
 export default function home({ route, navigation }) {
 
@@ -22,6 +24,7 @@ export default function home({ route, navigation }) {
     const [noOfSales, setnoOfSales] = useState('')
     const [noOfOrders, setnoOfOrders] = useState([])
     const [noOfSalesBox, setnoOfSalesBox] = useState(false)
+    const [detailsItem, setdetailsItem] = useState([])
 
 
     useEffect(() => {
@@ -48,7 +51,7 @@ export default function home({ route, navigation }) {
                 console.log(error)
             })
 
-        fetch(showSellerOrder_URL)
+        fetch(showSellerOrder_URL)              //non repeat
             .then((response) => response.json())
             .then((json) => {
                 while (noOfOrders.length > 0) {
@@ -56,35 +59,86 @@ export default function home({ route, navigation }) {
                 }
                 setnoOfOrders(json)
             })
+            .then()
             .catch((error) => {
                 console.log(error)
             })
+
     }, [])
 
-    // function OrderDetails(a) {
-    //     const showSellerOrderDetails_URL = 'http://192.168.1.66:3000/seller/home/show/order/details?seller_id=' + seller_home_id + '&order_id=' + a
+    const ModalPoup = ({ visible, children }) => {
+        const [showModal, setShowModal] = useState(visible)
+        return (
+            <Modal transparent visible={showModal}>
+                <View style={styles.modalBackGround}>
+                    <View style={styles.modalContainer}>
+                        {children}
+                    </View>
+                </View>
+            </Modal>
+        )
+    }
 
-    //     fetch(showSellerOrderDetails_URL)
-    //         .then((response) => response.json())
-    //         .then((json) => {
-    //             console.log(json)
-    //             console.log('success')
-    //         })
-    //         .catch((error) => {
-    //             console.log(error)
-    //         })
-    // }
+    const [visible, setVisible] = useState(false)
+
+
+
+
+    function OrderDetails(order_id) {
+        const tmpJsonarr = []
+        var tmpTotalArr = 0
+        const tmpPriceArr = []
+        var totalPrice = 0
+        const allinfoarr = []
+
+        const showSellerOrderDetails_URL = 'http://192.168.1.66:3000/seller/home/show/order/details?seller_id=' + seller_home_id + '&order_id=' + order_id
+        fetch(showSellerOrderDetails_URL)
+            .then((response) => response.json())
+            .then((json) => {
+                for (let i = 0; i < json.length; i++) {
+                    tmpJsonarr.push({
+                        name: json[i].product_name, 
+                        quantity: json[i].product_quantity
+                    })
+                    
+
+                    tmpTotalArr += json[i].product_quantity
+
+                    var p_price = json[i].product_price
+                    var convert_price = Number(p_price.replace(/[^0-9.-]+/g, ""))
+                    tmpPriceArr.push(convert_price)
+
+                    totalPrice += json[i].product_quantity*(tmpPriceArr[i])
+                }
+                while (allinfoarr.length > 0) {
+                    allinfoarr.pop()
+                }
+                for(let i=0; i<tmpJsonarr.length; i++) {
+                    allinfoarr.push(tmpJsonarr[i])
+                }
+                allinfoarr.push(tmpTotalArr)
+                allinfoarr.push("$ "+totalPrice.toFixed(2))
+                setdetailsItem(allinfoarr)
+                console.log(allinfoarr)
+                setVisible(true)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
     function Orderlist() {
-        // const showSellerOrderDetails_URL = 'http://192.168.1.66:3000/seller/home/show/order/details?seller_id=' + seller_home_id + '&order_id=' + {}
         return (
             <FlatList
                 keyExtractor={(item => item.order_id)}
                 data={noOfOrders}
                 renderItem={({ item }) =>
-                    <Card style={styles.EachSales}>
-                        <TouchableOpacity>
-                            <Text style={{ fontSize: 25, marginLeft: '3%', fontFamily: 'Montserrat-Bold' }}>Order ID: {item.order_id}</Text>
+                    <Card style={[styles.EachSales]}>
+
+                        <Text style={{ fontSize: 25, marginLeft: '3%', fontFamily: 'Montserrat-Bold' }}>Order ID: {item.order_id}</Text>
+
+                        <TouchableOpacity onPress={() => OrderDetails(item.order_id)}>
+                            <FontAwesomeIcon icon={faPlayCircle} size={32} style={{ color: '#5A9896', marginLeft: '60%' }} />
                         </TouchableOpacity>
                     </Card>}
             />
@@ -107,6 +161,17 @@ export default function home({ route, navigation }) {
             ) : null}
 
             <Orderlist></Orderlist>
+            <ModalPoup visible={visible}>
+                <View style={{ alignItems: 'center' }}>
+                    <View style={styles.header}>
+                        <TouchableOpacity onPress={() => setVisible(false)}>
+                            <FontAwesomeIcon icon={faWindowClose} size={32} />
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={{ textAlign: 'center' }}>HelloWorld</Text>                 
+                    <Text style={{ textAlign: 'center' }}>{detailsItem[0].name}</Text>
+                </View>
+            </ModalPoup>
         </SafeAreaView>
     )
 }
@@ -133,8 +198,27 @@ const styles = StyleSheet.create({
         marginRight: 'auto',
         backgroundColor: 'white',
         height: 80,
-        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
     },
+    modalBackGround: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalContainer: {
+        width: '80%',
+        height: '80%',
+        backgroundColor: 'white',
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+        borderRadius: 20,
+        elevation: 20
+    },
+    header: {
+        width: '100%',
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+    }
 })
-
-// style={{ fontSize: 25, textAlign: 'center', fontFamily: 'Montserrat-Regular', }}
